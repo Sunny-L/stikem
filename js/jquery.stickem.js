6d80ac4730b017fa7169822423c34d9c3b2e4816
Sunyson <260851801@qq.com>
2015-06-10 17:04:55 +0800

    add event

--- js/jquery.stickem.js
+++ js/jquery.stickem.js
@@ -1,16 +1,20 @@
 /*
  *	Created by Sunny.L on 2015/5/27	
- *	GitHub:https://github.com/Sunyson
+ *	GitHub:https://github.com/Sunny-L/stikem
 */
 
 ;(function($){
 	$.fn.extend({
 		stickem:function(options){
 			var options = $.extend({
-				
+				containerSelecter:'#stickem-container',
+				spend:200,
+				ev:function(){},							//传入的执行函数
+				eventExecute:1								//传入的执行次数
 			},options);
+			var evCounts = 0;
 			var self = this;
-			var tabsTop = $('#stickem-container').offset().top;//获取nav-tabs距离顶部的位置
+			var tabsTop = $(options.containerSelecter+'').offset().top;//获取nav-tabs距离顶部的位置
 			var clearScroll = 0;
 
 			$(window).on('scroll',function(){
@@ -19,10 +23,14 @@
 					var documentTop = $(document).scrollTop();//获取滚动条距离顶部距离
 					if(tabsTop<=documentTop){
 						$('.stickem').addClass('stickit');
+						if(evCounts<options.eventExecute){
+							options.ev();
+							evCounts++;
+						}
 					}else {
 						$('.stickem').removeClass('stickit');
 					}
-				},200);
+				},options.spend);
 			});
 		}
 	});
