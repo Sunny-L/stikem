b016596155c9c50ee8897d5874ca5265a248a25e
sunny <260851801@qq.com>
2015-10-15 14:48:37 +0800

    优化

--- js/jquery.stickem.js
+++ js/jquery.stickem.js
@@ -1,5 +1,5 @@
 /*
- *	Created by Sunny.L on 2015/5/27	
+ *	Created by Sunny.L on 2015/5/27
  *	GitHub:https://github.com/Sunny-L/stikem
 */
 
@@ -8,9 +8,10 @@
 		stickem:function(options){
 			var options = $.extend({
 				containerSelecter:'#stickem-container',
-				spend:200,
-				ev:function(){},							//传入的执行函数
-				eventExecute:1								//传入的执行次数
+				spend:0,
+				ev:null,							//传入的执行函数
+				eventExecute:1,								//传入的执行次数
+				outContainerEv:null				//元素恢复时执行事件
 			},options);
 			var evCounts = 0;
 			var self = this;
@@ -18,20 +19,32 @@
 			var clearScroll = 0;
 
 			$(window).on('scroll',function(){
-				clearTimeout(clearScroll);
-				clearScroll = setTimeout(function(){
-					var documentTop = $(document).scrollTop();//获取滚动条距离顶部距离
-					if(tabsTop<=documentTop){
-						$('.stickem').addClass('stickit');
+				if(!options.speend){
+					scrollFn();
+				}else{
+					clearTimeout(clearScroll);
+					clearScroll = setTimeout(scrollFn,options.spend);
+				}
+
+			});
+			var scrollFn = function(){
+				var documentTop = $(document).scrollTop();//获取滚动条距离顶部距离
+				if(tabsTop<=documentTop){
+					$('.stickem').addClass('stickit');
+					if(options.ev){
 						if(evCounts<options.eventExecute){
 							options.ev();
 							evCounts++;
 						}
-					}else {
-						$('.stickem').removeClass('stickit');
 					}
-				},options.spend);
-			});
+
+				}else {
+					$('.stickem').removeClass('stickit');
+					if(options.outContainerEv){
+						options.outContainerEv();
+					}
+				}
+			}
 		}
 	});
 })(jQuery);
